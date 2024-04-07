461d4b6f01695a14e16d7a7dd483404e21094a90
Krutyash <kostrov2525@gmail.com>
2024-04-07 15:27:05 +0500

    initial commit

---
+++ SpaceSurvival/Game1.cs
@@ -0,0 +1,53 @@
+﻿namespace SpaceSurvival;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = 1024;
+        _graphics.PreferredBackBufferHeight = 768;
+        _graphics.ApplyChanges();
+
+        Globals.Content = Content;
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        Globals.SpriteBatch = _spriteBatch;
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        Globals.Update(gameTime);
+
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        
+        _spriteBatch.Begin();
+        
+        _spriteBatch.End();
+        
+        base.Draw(gameTime);
+    }
+}
---
+++ SpaceSurvival/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new SpaceSurvival.Game1();
+game.Run();
